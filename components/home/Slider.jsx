8996b3d8aa51d38957ce62e807631df4e053cbe6
gareginra <40554131+gareginra@users.noreply.github.com>
2022-01-03 21:00:25 +0400

    test

--- components/home/Slider.jsx
+++ components/home/Slider.jsx
@@ -3,7 +3,7 @@
 import React, { Component } from "react";
 import Image from "next/image";
 import sliderData from "/data/files/slider-images.js";
-
+import testPic from "/data/files/images/slider/1.jpg";
 const path = () => sliderData[0].url;
 
 // const MyImage = (props) => {
@@ -19,14 +19,43 @@ const path = () => sliderData[0].url;
 //   }
 
 const Slider = () => {
+    console.log(sliderData)
     return (
         <>
-            <img src={sliderData[0].url} />
+            <div className="slider-background">
+                <div className="left slide">
+                    <Image src={testPic} />
+                </div>
+                <div className="mainSlide">
+
+                </div>
+                <div className="right slide">
+                    <Image src={testPic} />
+                </div>
+            </div>
             <style jsx>{` 
                 .slider-background{
+                    display: flex;
+                    position: relative;
                     width: 100%;
                     min-height:300px;
                     background:brown;
+                    z-index:-1;
+                    margin-top: 50px;
+                }
+                .slide {
+                    display:block;
+                    position: relative;
+                    width: 33vw;
+                    min-height:300px;
+                    background-color: black;
+                    opacity: 10%;
+                }
+                .left {
+                    float: left;
+                }
+                .right {
+                    margin-left: auto;
                 }
             `}</style>
         </>
