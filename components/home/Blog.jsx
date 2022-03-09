5404aa0febc9a3dc1547955796a4510a2539f5db
Denis Balandin <denisbalandin@Deniss-MacBook-Pro.local>
2022-03-09 12:03:14 -0800

    update for map fix problem

--- components/home/Blog.jsx
+++ components/home/Blog.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import MostRead from "./MostRead";
 import Post from "./Post";
 import BlogService from "../../lib/services/BlogService";
-const Blog = ({page}) => {
+const Blog = ({ page }) => {
   const [blogData, setBlogData] = useState([]);
   useEffect(() => {
     const blogCheckResponse = async () => {
@@ -13,61 +13,28 @@ const Blog = ({page}) => {
     };
     blogCheckResponse();
   }, []);
-  return (blogData[0] ?
+  return (
     <div className="blog-background">
       <div className="list">
-        <div className="mob">
-          {blogData.slice(0,10).map((item) => (
-            <Post data={item} mob/>
-          ))}
-        </div>
         <div className="first">
-            <div className="post-list">
-              {blogData.slice(0,10).map((item) => (
-                <Post data={item} />
-              ))}
-            </div>
-            {/* <div className="ad">Advertisement</div> */}
+          <div className="post-list">
+            {blogData &&
+              blogData
+                .slice(0, 10)
+                .map((item) => <Post key={item.id} data={item} />)}
+          </div>
         </div>
         <div className="most-read">
-            <MostRead />
+          <MostRead />
         </div>
         <div className="second">
-            <div className="post-list">
-              {blogData.slice(10,20).map((item) => (
-                <Post data={item} />
-              ))}
-            </div>
-            {/* <div className="ad">Advertisement</div> */}
+          <div className="post-list">
+            {blogData &&
+              blogData
+                .slice(10, 20)
+                .map((item) => <Post key={item.id} data={item} />)}
+          </div>
         </div>
-        {/* <div className="half second">
-                    <div className=" post-list">
-                        <div className="row">
-                            <Post id={5} />
-                            <Post id={6} />
-                            <Post id={6} />
-                        </div>
-                        <div className="row">
-                            <Post id={7} />
-                            <Post id={8} />
-                            <Post id={8} />
-                        </div>
-                    </div>
-                    <div className="three post-list">
-                        <div className="row">
-                            <Post id={0} />
-                            <Post id={5} />
-                            <Post id={1} />
-                            <Post id={1} />
-                        </div>
-                        <div className="row">
-                            <Post id={4} />
-                            <Post id={2} />
-                            <Post id={7} />
-                            <Post id={3} />
-                        </div>
-                    </div>
-                </div> */}
       </div>
       <style jsx>{`
         @media (min-width: 0px) {
@@ -106,26 +73,22 @@ const Blog = ({page}) => {
             flex-wrap: wrap;
             justify-items: center;
           }
-          // .ad {
-          //   margin: 2rem 0 0 0;
-          //   width: 20rem;
-          //   border: 1px dashed green;
-          // }
-          .second {
-            margin-top: 20rem;
-          }
+        }
+        .first {
+          max-width: 75rem;
+          margin: 0 auto;
+        }
+        .second {
+          max-width: 75rem;
+          margin: 0 auto;
         }
         .blog-background {
           width: 100%;
           display: flex;
           justify-content: center;
         }
-        .most-read {
-          height: 100px;
-        }
       `}</style>
     </div>
-    :<></>
   );
 };
 
